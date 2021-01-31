c9eb0a7e62b796bfe2ac29719b50e98ea9e30d35
Sandrine <didine21@yahoo.com>
2021-01-31 16:29:12 +0000

    changes sunday 31/01

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-function compute()
-{
+function compute() {
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = parseInt(document.getElementById("nbYears").value);
-    var interest = principal*years*rate/100;
-    var today = new Date();
-    var yyyy = today.getFullYear();
-    var actualYear = yyyy+years;
-    var textResult = "If you deposit <mark>"+ principal +"</mark>,<br>at an interest rate of <mark>"+ rate + "</mark>%.";
-    textResult= textResult+"<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>" + actualYear +"</mark>";
-    document.getElementById('results').innerHTML=textResult;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+    } else {
+        var rate = document.getElementById("rate").value;
+        var years = parseInt(document.getElementById("nbYears").value);
+        var interest = principal * years * rate / 100;
+        var today = new Date();
+        var yyyy = today.getFullYear();
+        var actualYear = yyyy + years;
+        var textResult = "If you deposit <mark>" + principal + "</mark>,<br>at an interest rate of <mark>" + rate + "</mark>%.";
+        textResult = textResult + "<br>You will receive an amount of <mark>" + interest + "</mark>,<br>in the year <mark>" + actualYear + "</mark>";
+        document.getElementById('results').innerHTML = textResult;
+    }
 }
 function updateRateValue(val) {
-    document.getElementById('rateVal').value=val; 
+    document.getElementById('rateVal').value = val;
 }
 function checkPrincipalValue(val) {
-    if (val<=0) {
-        alert("Enter a positive number"); 
+    if (val <= 0) {
+        alert("Enter a positive number");
     }
 }
