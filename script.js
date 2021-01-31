881e8a37173ca47db494489ba51580a0c468f956
Sandrine <didine21@yahoo.com>
2021-01-31 15:23:02 +0000

    changes sunday 31/01

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("nbYears").value;
+    interest = principal*years*rate/100;
+    var today = new Date();
+    var yyyy = today.getFullYear();
+    actualYear = yyyy+years;
+    textResult = "if you deposit " + "principal";
+    document.getElementById('results').innerHTML=textresult;
 }
-        
+function updateRateValue(val) {
+    document.getElementById('rateVal').value=val; 
+}
+function checkPrincipalValue(val) {
+    if (val<=0) {
+        alert("Enter a positive number"); 
+    }
+}
